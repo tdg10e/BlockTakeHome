4c3795e8acfbe4bcb37d5b08d161418c04f534c0
tdg10e <tremaine.grant@gmail.com>
2023-07-18 19:43:30 -0400

    Initial Commit

---
+++ BlockTakeHome/BlockTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockTakeHomeApp.swift
+//  BlockTakeHome
+//
+//  Created by Tremaine Grant on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct BlockTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlockTakeHome/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BlockTakeHome
+//
+//  Created by Tremaine Grant on 7/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
